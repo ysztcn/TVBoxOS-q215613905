4940c3c5edbcc779ee1b7bf5caf6578c645db34d
于俊 <215613905@qq.com>
2026-06-25 17:55:16 +0800

    修正一处哔哩播放问题

--- app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
+++ app/src/main/java/com/github/tvbox/osc/viewmodel/SourceViewModel.java
@@ -44,6 +44,7 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import org.greenrobot.eventbus.EventBus;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 
